592d81b8a4c0191e414da04dd99634c004cc7275
Denis Senchenko <denissenchenko@yandex.com>
2017-08-23 18:40:57 +0200

    Added new markdown mode for comment highlighting

---
+++ src/js/codemirror/mode/comment/comment.js
@@ -0,0 +1,31 @@
+﻿(function (mod) {
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+        mod(require("../../../../../node_modules/codemirror/lib/codemirror"));
+    else if (typeof define == "function" && define.amd) // AMD
+        define(["../../../../../node_modules/codemirror/lib/codemirror"], mod);
+    else // Plain browser env
+        mod(CodeMirror);
+})
+(function (CodeMirror) {
+    "use strict";
+    CodeMirror.defineMode("comment",
+        function() {
+
+            return {
+                token: function(stream, state) {
+                    if (stream.match("&&&")) {
+                        if (!stream.eatWhile(/[\w\s]/)) {
+                            return null;
+                        }
+
+                        if (stream.match("&&&")) {
+                            return "style2";
+                        }
+                    }
+                    while (stream.next() != null && !stream.match("&&&", false)) { }
+                    return null;
+                }
+            };
+
+        });
+});
